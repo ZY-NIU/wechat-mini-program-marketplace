351934d2adecf6af98220dcbd8ce20e3bd9330a5
Zeyu Niu <101955783+ZY-NIU@users.noreply.github.com>
2023-07-23 21:23:50 -0400

    add selling page

--- pages/selling/index.js
+++ pages/selling/index.js
@@ -1,66 +1,21 @@
-// pages/selling/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    userHeadUrl: 'https://wx.qlogo.cn/mmopen/vi_32/5mKrvn3ibyDNaDZSZics3aoKlz1cv0icqn4EruVm6gKjsK0xvZZhC2hkUkRWGxlIzOEc4600JkzKn9icOLE6zjgsxw/132',
+  
+    isCreateTradePopupShow: false,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  createTrade(e) {
+    this.setData({
+      isCreateTradePopupShow: true,
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  handlePopupHide() {
+    this.setData({
+      isCreateTradePopupShow: false,
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
