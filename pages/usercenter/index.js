cb4a6a263bab072de51cc31e971f19988fb6afa2
Zeyu Niu <101955783+ZY-NIU@users.noreply.github.com>
2023-07-30 02:27:28 -0400

    add usercenter

--- pages/usercenter/index.js
+++ pages/usercenter/index.js
@@ -1,99 +1,25 @@
-import { fetchUserCenter } from '../../services/usercenter/fetchUsercenter';
-import Toast from 'tdesign-miniprogram/toast/index';
-
-const menuData = [
-  [
-    {
-      title: '收货地址',
-      tit: '',
-      url: '',
-      type: 'address',
-    },
-    {
-      title: '优惠券',
-      tit: '',
-      url: '',
-      type: 'coupon',
-    },
-    {
-      title: '积分',
-      tit: '',
-      url: '',
-      type: 'point',
-    },
-  ],
-  [
-    {
-      title: '帮助中心',
-      tit: '',
-      url: '',
-      type: 'help-center',
-    },
-    {
-      title: '客服热线',
-      tit: '',
-      url: '',
-      type: 'service',
-      icon: 'service',
-    },
-  ],
-];
-
-const orderTagInfos = [
-  {
-    title: '待付款',
-    iconName: 'wallet',
-    orderNum: 0,
-    tabType: 5,
-    status: 1,
-  },
-  {
-    title: '待发货',
-    iconName: 'deliver',
-    orderNum: 0,
-    tabType: 10,
-    status: 1,
-  },
-  {
-    title: '待收货',
-    iconName: 'package',
-    orderNum: 0,
-    tabType: 40,
-    status: 1,
-  },
-  {
-    title: '待评价',
-    iconName: 'comment',
-    orderNum: 0,
-    tabType: 60,
-    status: 1,
-  },
-  {
-    title: '退款/售后',
-    iconName: 'exchang',
-    orderNum: 0,
-    tabType: 0,
-    status: 1,
-  },
-];
-
 const getDefaultData = () => ({
-  showMakePhone: false,
-  userInfo: {
-    avatarUrl: '',
-    nickName: '正在登录...',
-    phoneNumber: '',
-  },
-  menuData,
-  orderTagInfos,
-  customerServiceInfo: {},
-  currAuthStep: 1,
-  showKefu: true,
-  versionNo: '',
+  
 });
 
 Page({
-  data: getDefaultData(),
+  data: {
+    showMakePhone: false,
+    userInfo: {
+      avatarUrl: '',
+      nickName: '正在登录...',
+      phoneNumber: '',
+    },
+    // menuData,
+    // orderTagInfos,
+    customerServiceInfo: {},
+    currAuthStep: 1,
+    showKefu: true,
+    versionNo: '',
+    following: 0,
+    follower: 0,
+    save: 0,
+  },
 
   onLoad() {
     this.getVersionInfo();
@@ -103,6 +29,7 @@ Page({
     this.getTabBar().init();
     this.init();
   },
+
   onPullDownRefresh() {
     this.init();
   },
@@ -112,36 +39,36 @@ Page({
   },
 
   fetUseriInfoHandle() {
-    fetchUserCenter().then(
-      ({
-        userInfo,
-        countsData,
-        orderTagInfos: orderInfo,
-        customerServiceInfo,
-      }) => {
-        // eslint-disable-next-line no-unused-expressions
-        menuData?.[0].forEach((v) => {
-          countsData.forEach((counts) => {
-            if (counts.type === v.type) {
-              // eslint-disable-next-line no-param-reassign
-              v.tit = counts.num;
-            }
-          });
-        });
-        const info = orderTagInfos.map((v, index) => ({
-          ...v,
-          ...orderInfo[index],
-        }));
-        this.setData({
-          userInfo,
-          menuData,
-          orderTagInfos: info,
-          customerServiceInfo,
-          currAuthStep: 2,
-        });
-        wx.stopPullDownRefresh();
-      },
-    );
+    // fetchUserCenter().then(
+    //   ({
+    //     userInfo,
+    //     countsData,
+    //     orderTagInfos: orderInfo,
+    //     customerServiceInfo,
+    //   }) => {
+    //     // eslint-disable-next-line no-unused-expressions
+    //     menuData?.[0].forEach((v) => {
+    //       countsData.forEach((counts) => {
+    //         if (counts.type === v.type) {
+    //           // eslint-disable-next-line no-param-reassign
+    //           v.tit = counts.num;
+    //         }
+    //       });
+    //     });
+    //     const info = orderTagInfos.map((v, index) => ({
+    //       ...v,
+    //       ...orderInfo[index],
+    //     }));
+    //     this.setData({
+    //       userInfo,
+    //       menuData,
+    //       orderTagInfos: info,
+    //       customerServiceInfo,
+    //       currAuthStep: 2,
+    //     });
+    //     wx.stopPullDownRefresh();
+    //   },
+    // );
   },
 
   onClickCell({ currentTarget }) {
@@ -223,11 +150,11 @@ Page({
 
   gotoUserEditPage() {
     const { currAuthStep } = this.data;
-    if (currAuthStep === 2) {
+    // if (currAuthStep === 2) {
       wx.navigateTo({ url: '/pages/usercenter/person-info/index' });
-    } else {
-      this.fetUseriInfoHandle();
-    }
+    // } else {
+    //   this.fetUseriInfoHandle();
+    // }
   },
 
   getVersionInfo() {
