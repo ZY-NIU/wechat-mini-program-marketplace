42392f1e45f3ecbe2b866a2588af2b260879586d
Zeyu Niu <101955783+ZY-NIU@users.noreply.github.com>
2023-07-30 18:54:16 -0400

    add goods short list

--- components/goods-short-list/index.js
+++ components/goods-short-list/index.js
@@ -1,23 +1,62 @@
-// components/goods-short-list/index.js
 Component({
-  /**
-   * 组件的属性列表
-   */
+  externalClasses: ['wr-class'],
+
   properties: {
-
+    goodsList: {
+      type: Array,
+      value: [],
+    },
+    id: {
+      type: String,
+      value: '',
+      observer: (id) => {
+        this.genIndependentID(id);
+      },
+    },
+    thresholds: {
+      type: Array,
+      value: [],
+    },
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    independentID: '',
   },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+  lifetimes: {
+    ready() {
+      this.init();
+    },
+  },
 
-  }
-})
+  methods: {
+    onClickGoods(e) {
+      const { index } = e.currentTarget.dataset;
+      this.triggerEvent('click', { ...e.detail, index });
+    },
+
+    onAddCart(e) {
+      const { index } = e.currentTarget.dataset;
+      this.triggerEvent('addcart', { ...e.detail, index });
+    },
+
+    onClickGoodsThumb(e) {
+      const { index } = e.currentTarget.dataset;
+      this.triggerEvent('thumb', { ...e.detail, index });
+    },
+
+    init() {
+      this.genIndependentID(this.id || '');
+    },
+
+    genIndependentID(id) {
+      if (id) {
+        this.setData({ independentID: id });
+      } else {
+        this.setData({
+          independentID: `goods-list-${~~(Math.random() * 10 ** 8)}`,
+        });
+      }
+    },
+  },
+});
--- pages/goods/lists/index.js
+++ pages/goods/lists/index.js
@@ -1,66 +1,94 @@
-// pages/goods/lists/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    userName: "Zeyu",
+    userHeadUrl: 'https://wx.qlogo.cn/mmopen/vi_32/5mKrvn3ibyDNaDZSZics3aoKlz1cv0icqn4EruVm6gKjsK0xvZZhC2hkUkRWGxlIzOEc4600JkzKn9icOLE6zjgsxw/132',
+    
+    goodsList: [
+      {
+        id: 1,
+        title: '白色短袖连衣裙发撒的发撒地方阿斯顿发送到发送的发',
+        price: 34,
+        originPrice: 35,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/nz-09a.png',
+      },
+      {
+        id: 2,
+        title: '短袖',
+        price: 3456,
+        originPrice: 9999,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/dz-3a.png',
+      },
+      {
+        id: 3,
+        title: '棋',
+        price: 4,
+        originPrice: 3,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/dz-2a.png',
+      },
+      {
+        id: 4,
+        title: '5个碗，6个锅',
+        price: 4,
+        originPrice: 3,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/nz-17a.png',
+      },
+      {
+        id: 5,
+        title: 'personal computer, mouse',
+        price: 4,
+        originPrice: 3,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/nz-09a.png',
+      },
+      {
+        id: 6,
+        title: '洗衣机，烘干机，衣服，裤子',
+        price: 4,
+        originPrice: 3,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/dz-3a.png',
+      },
+      {
+        id: 11,
+        title: '白色短袖连衣裙发撒的发撒地方阿斯顿发送到发送的发',
+        price: 34,
+        originPrice: 35,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/nz-09a.png',
+      },
+      {
+        id: 21,
+        title: '短袖',
+        price: 3456,
+        originPrice: 9999,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/dz-3a.png',
+      },
+      {
+        id: 31,
+        title: '棋',
+        price: 4,
+        originPrice: 3,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/dz-2a.png',
+      },
+      {
+        id: 41,
+        title: '5个碗，6个锅',
+        price: 4,
+        originPrice: 3,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/nz-17a.png',
+      },
+      {
+        id: 51,
+        title: 'personal computer, mouse',
+        price: 4,
+        originPrice: 3,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/nz-09a.png',
+      },
+      {
+        id: 61,
+        title: '洗衣机，烘干机，衣服，裤子asdfasdf大神发四大发阿斯顿发撒地方撒地方撒地方撒地方阿斯顿发阿斯顿发阿斯顿发四大发阿斯顿发阿斯顿发撒地方四大发撒地方撒地方撒的发撒的发撒的发达',
+        price: 4,
+        originPrice: 3,
+        priImg: 'https://cdn-we-retail.ym.tencent.com/tsr/goods/dz-3a.png',
+      },
+    ]
   }
+
 })
