257981f48d964763fb8ae0765f052a36be40868d
Zeyu Niu <101955783+ZY-NIU@users.noreply.github.com>
2023-07-31 21:47:20 -0400

    add chat page

---
+++ pages/message/chat/index.js
@@ -0,0 +1,68 @@
+Page({
+
+  data: {
+    myInfo: {
+      id: 1,
+      avatar: 'https://wx.qlogo.cn/mmopen/vi_32/5mKrvn3ibyDNaDZSZics3aoKlz1cv0icqn4EruVm6gKjsK0xvZZhC2hkUkRWGxlIzOEc4600JkzKn9icOLE6zjgsxw/132',
+      username: "的发顺丰",
+    },
+    otherInfo: {
+      id: 2,
+      avatar: 'https://wx.qlogo.cn/mmopen/vi_32/5mKrvn3ibyDNaDZSZics3aoKlz1cv0icqn4EruVm6gKjsK0xvZZhC2hkUkRWGxlIzOEc4600JkzKn9icOLE6zjgsxw/132',
+      username: "Zeyu",
+    },
+    
+    chatList: [
+      {
+        id: 1,
+        text: "发撒地方啊的撒发阿斯顿发",
+        time: "2023/07/16 21:34"
+      },
+      {
+        id: 1,
+        text: "发撒地方啊的撒fadsfasdfasd发阿斯顿发",
+        time: "2023/07/16 21:34"
+      },
+      {
+        id: 2,
+        text: "fasdfadsf",
+        time: "2023/07/16 21:34"
+      },
+      {
+        id: 2,
+        text: "dsafasdfasd阿斯顿发阿斯顿发阿斯顿发阿斯顿 阿斯顿发阿斯顿发阿斯顿发阿斯顿发阿斯顿发阿斯顿发啊舒服撒地方",
+        time: "2023/07/16 21:34"
+      },
+      {
+        id: 1,
+        text: "阿斯顿发撒地方asdfasdfasdf阿方索点点滴滴点点滴滴点点滴滴的等待刀的点点滴滴刀的点点滴滴的点点滴滴点点滴滴点点滴滴刀",
+        time: "2023/07/16 21:34"
+      },
+      {
+        id: 2,
+        text: "阿斯顿发阿斯顿发",
+        time: "2023/07/16 21:34"
+      },
+      {
+        id: 1,
+        text: "阿斯顿发阿斯顿发阿斯顿发",
+        time: "2023/07/16 21:34"
+      },
+      {
+        id: 2,
+        text: "啊发大水发阿斯顿发啊的撒发",
+        time: "2023/07/16 21:34"
+      },
+      {
+        id: 1,
+        text: "阿斯顿发阿斯顿发阿斯顿发阿斯顿发",
+        time: "2023/07/16 21:34"
+      },
+      {
+        id: 2,
+        text: "yyyyyyy隐隐约约有意义又犹犹豫豫犹犹豫豫犹犹豫豫犹犹豫豫犹犹豫豫月隐隐约约 ",
+        time: "2023/07/16 21:34"
+      },
+    ]
+  },
+})
--- pages/message/index.js
+++ pages/message/index.js
@@ -5,7 +5,70 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    messages: [
+      {
+        userAvatar: 'https://wx.qlogo.cn/mmopen/vi_32/5mKrvn3ibyDNaDZSZics3aoKlz1cv0icqn4EruVm6gKjsK0xvZZhC2hkUkRWGxlIzOEc4600JkzKn9icOLE6zjgsxw/132',
+        username: "的发顺丰",
+        record: [
+          {
+            text: "aaaaa",
+            time: "1/7",
+          }
+        ]
+      },
+      {
+        userAvatar: 'https://wx.qlogo.cn/mmopen/vi_32/5mKrvn3ibyDNaDZSZics3aoKlz1cv0icqn4EruVm6gKjsK0xvZZhC2hkUkRWGxlIzOEc4600JkzKn9icOLE6zjgsxw/132',
+        username: "阿斯顿发",
+        record: [
+          {
+            text: "hello啊的撒发阿斯顿发阿斯顿发阿斯顿发撒地方撒地方撒地方撒地方四大发",
+            time: "11/12",
+          }
+        ]
+      },
+      {
+        userAvatar: 'https://wx.qlogo.cn/mmopen/vi_32/5mKrvn3ibyDNaDZSZics3aoKlz1cv0icqn4EruVm6gKjsK0xvZZhC2hkUkRWGxlIzOEc4600JkzKn9icOLE6zjgsxw/132',
+        username: "啊撒",
+        record: [
+          {
+            text: "你好呀",
+            time: "15:12",
+          }
+        ]
+      },
+      {
+        userAvatar: 'https://wx.qlogo.cn/mmopen/vi_32/5mKrvn3ibyDNaDZSZics3aoKlz1cv0icqn4EruVm6gKjsK0xvZZhC2hkUkRWGxlIzOEc4600JkzKn9icOLE6zjgsxw/132',
+        username: "Zeyu",
+        record: [
+          {
+            text: "还在出吗",
+            time: "13:10",
+          }
+        ]
+      },
+      {
+        userAvatar: 'https://wx.qlogo.cn/mmopen/vi_32/5mKrvn3ibyDNaDZSZics3aoKlz1cv0icqn4EruVm6gKjsK0xvZZhC2hkUkRWGxlIzOEc4600JkzKn9icOLE6zjgsxw/132',
+        username: "Zeyu",
+        record: [
+          {
+            text: "aaaaa",
+            time: "11/7",
+          },
+          {
+            text: "aaaaa",
+            time: "11/7",
+          },
+          {
+            text: "还好",
+            time: "11/7",
+          },
+          {
+            text: "出完了吗",
+            time: "11/7",
+          }
+        ]
+      }
+    ],
   },
 
   /**
